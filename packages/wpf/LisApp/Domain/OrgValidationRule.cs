f21506d6c17b378d880edee77e3b30e8e3de482c
Alexey Grinevich <alexey.grinevich@grinmark.com>
2023-09-19 09:57:38 +0300

    Use publishing instead of vdproj setup

---
+++ packages/wpf/LisApp/Domain/OrgValidationRule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace LisApp.Domain;
+
+public class OrgValidationRule : ValidationRule
+{
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        string str = "" + value;
+        if( string.IsNullOrWhiteSpace(str) )
+        {
+            return ValidationResult.ValidResult;
+        }
+        bool valid = str.All(char.IsLetterOrDigit) || Uri.IsWellFormedUriString(str, UriKind.Absolute);
+
+        return !valid
+            ? new ValidationResult(false, "Organization name or URL is expected")
+            : ValidationResult.ValidResult;
+    }
+}
