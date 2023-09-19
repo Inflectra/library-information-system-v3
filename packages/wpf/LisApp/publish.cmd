310383e2de2f03a1a69e357176ed40a974fa8ca9
Alexey Grinevich <alexey.grinevich@grinmark.com>
2023-09-19 10:24:16 +0300

    Publishing fine-tuned

--- packages/wpf/LisApp/publish.cmd
+++ packages/wpf/LisApp/publish.cmd
@@ -1,4 +1,4 @@
 pushd %~dp0
-dotnet publish -c Release -o ..\publish\ -r win-x64 --self-contained true -p:PublishTrimmed=true -p:PublishReadyToRun=true
+dotnet publish -p:PublishProfile=FolderProfile
 "c:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.35.32215\bin\Hostx64\x64\editbin.exe" /subsystem:windows ..\publish\LisApp.exe
 popd
