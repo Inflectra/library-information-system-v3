3cb045b14aa62d0a307585da751a7463e7419771
Alexey Grinevich <alexey.grinevich@grinmark.com>
2023-11-13 11:47:27 +0300

    CreateBook default values

--- packages/reactui/src/app/pages/BookEdit.jsx
+++ packages/reactui/src/app/pages/BookEdit.jsx
@@ -55,7 +55,7 @@ function BookEdit(props) {
         .then((genres)=>{
             genres.sort(nameSort);
             setGenreList(genres)
-            if(id===0) {
+            if(!id) {
                 if(genres&&genres.length) {
                     setGenre(genres[0].id)
                 }    
@@ -65,10 +65,10 @@ function BookEdit(props) {
         .then((authors)=>{
             authors.sort(nameSort);
             setAuthorList(authors)
-            if(id===0) {
+            if(!id) {
                 if(authors&&authors.length) {
                     setAuthor(authors[0].id)
-                }    
+                }
             }
         })
     }, [id,props])
